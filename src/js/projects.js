28b5018e19c755b745ca1754db1ef32e474921d7
Carter Irish <2023c.irish@gmail.com>
2025-08-30 19:20:55 -0400

    Refactor project fetching logic by removing unnecessary DOMContentLoaded wrapper

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,24 +1,22 @@
 const dataPath = './src/data/projects.json';
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Fetch and display projects from the JSON file
-  fetch(dataPath)
-    .then(response => response.json())
-    .then(data => {
-      // Get projects container
-      const projectsContainer = document.getElementById('projects-container');
-      // Create HTML for each project
-      const projectsHtml = data.map(project => `
-        <div class="project-card">
-          <img src="${project.image}" alt="${project.Title}" class="project-image"/>
-          <h2 class="project-title">${project.Title}</h2>
-          <p class="project-description">${project.description}</p>
-          <a href="${project.link}" class="project-link" target="_blank" rel="noopener noreferrer">View Project</a>
-        </div>
-      `).join('');
+// Fetch and display projects from the JSON file
+fetch(dataPath)
+  .then(response => response.json())
+  .then(data => {
+    // Get projects container
+    const projectsContainer = document.getElementById('projects-container');
+    // Create HTML for each project
+    const projectsHtml = data.map(project => `
+      <div class="project-card">
+        <img src="${project.image}" alt="${project.Title}" class="project-image"/>
+        <h2 class="project-title">${project.Title}</h2>
+        <p class="project-description">${project.description}</p>
+        <a href="${project.link}" class="project-link" target="_blank" rel="noopener noreferrer">View Project</a>
+      </div>
+    `).join('');
 
-      // Insert projects HTML into the container
-      projectsContainer.innerHTML = projectsHtml;
+    // Insert projects HTML into the container
+    projectsContainer.innerHTML = projectsHtml;
 
-    });
-});
+  });
